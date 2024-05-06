ec515fa89cde21f8c8c71de9aa8d1e5f40a20995
Miguel Ruiz <later481@gmail.com>
2024-05-05 23:06:11 -0600

    Mostrar resultados en UI

--- app.js
+++ app.js
@@ -32,12 +32,30 @@ function buscarImagenes() {
         .then(respuesta => respuesta.json())
         .then(resultado => {
             const {data} = resultado;
-            const gifs = data.map( img => ({
-                id: img.id,
-                title: img.title,
-                url: img.images.downsized_medium.url,
-            }));
-            console.log(gifs);
+            mostrarImagenes(data)
         })
+}
 
+function mostrarImagenes(datos){
+    //remueve el hijo si es que existe
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+    //Creacion de hijos - imagenes
+    datos.forEach(dato => {
+        const {id, title, images:{downsized_medium:{url}}, images:{downsized_large:{urlLarge = url}}} = dato;
+        //console.log(url);
+        resultado.innerHTML += `
+            <div class="gap-4">
+                <div class="bg-white rounded-lg border border-gray-200 shadow cardDetails">
+                    <img class="w-full h-auto" src="${url}">
+                    <div class="p-4">
+                        <p class="font-bold"><span class="font-light">${title}</span></p>
+
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1 transition duration-300 ease-in-out hover:opacity-100" href="${urlLarge}" target="_blank" rel="noopener noreferrer">Ver Gif</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
 }
