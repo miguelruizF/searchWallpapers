e2a4bdd6a4117778a4487bab67598e9d2a03c119
Miguel Ruiz <later481@gmail.com>
2024-05-06 23:06:30 -0600

    Funcion de paginacion

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const d = document;
 const resultado = d.querySelector("#resultados");
 const paginacion = d.querySelector("#paginacion");
 const formulario =  d.querySelector("#formulario");
+const limiteImages = 20;
+let totalPaginas;
+let iterador;
+let paginaActual = 1;
 
 window.onload = () =>{
     formulario.addEventListener("submit", validarFormulario);
@@ -25,16 +29,31 @@ function validarFormulario(e){
 function buscarImagenes() {
     const termino = d.querySelector('#default-search').value;
     const key = API_KEY;
-    const url = `https://api.giphy.com/v1/stickers/search?api_key=${key}&q=${termino}`; 
+    const url = `https://api.giphy.com/v1/stickers/search?api_key=${key}&q=${termino}&limit=${limiteImages}&offset=${paginaActual}`; 
     // console.log(url);
     //Fetch a la url
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
+            //console.log(resultado)
+            totalPaginas = calcularPaginas(resultado.pagination.total_count)
+            // console.log(resultado.pagination.total_count)
             const {data} = resultado;
             mostrarImagenes(data)
         })
 }
+
+//Generador de paginas
+function *crearPaginador(total){
+    for(let i =1; i<=total; i++){
+        yield i;
+    }
+}
+
+function calcularPaginas(total) {
+    return parseInt(Math.ceil(total / limiteImages));
+}
+
 
 function mostrarImagenes(datos){
     //remueve el hijo si es que existe
@@ -58,4 +77,31 @@ function mostrarImagenes(datos){
             </div>
         `
     });
+
+    //Limpiar paginacion
+    while(paginacion.firstChild){
+        paginacion.removeChild(paginacion.firstChild);
+    }
+    //Generar paginador
+    imprimirPaginador();
+}
+
+function imprimirPaginador(){
+    iterador = crearPaginador(totalPaginas);
+    while(true){
+        const {value, done} = iterador.next();
+        if(done) return;
+        //Caso contrario, generar botones
+        const boton = d.createElement("a");
+        boton.href="#";
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-blue-800', 'hover:bg-blue-500', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-10', 'rounded', 'text-white');
+        
+        boton.onclick = () =>{
+            paginaActual = value;
+            buscarImagenes();
+        }
+        paginacion.appendChild(boton);
+    }
 }
